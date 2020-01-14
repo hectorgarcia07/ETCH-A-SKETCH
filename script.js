8ced8363670effdcbd63d2d4c27434e4f71997be
Hector Garcia <hector.garcia07@utrgv.edu>
2020-01-13 23:50:45 -0600

    fixed bugs and refactored javascript

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var parent = document.getElementById("etchBox");
 var number = 5;
 var resetButton = document.getElementById("reset");
 var resolution = document.getElementById("resolution");
-var pixelColor = document.getElementById("pixelColor");
-var rgbColor = "rgb(0, 0, 0)";
 var colorStyle = document.getElementById("colorStyle");
+var isGreyScale = false;
+var cells = [];
+var shadeBlack = ["rgb(230, 230, 230)", "rgb(205, 205, 205)",
+                  "rgb(180, 180, 180", "rgb(155, 155, 155)",
+                  "rgb(130, 130, 130)", "rgb(105, 105, 105)",
+                  "rgb(80, 80, 80)", "rgb(55, 55, 55)",
+                  "rgb(30, 30, 30)", "rgb(0, 0, 0)"
+                    ];
 
 //if button clicked, reset and prompt the user for 
 //a new resolution
@@ -52,6 +58,7 @@ function removeNodeChild()
 {
     let allCol = document.getElementById("etchBox");
     allCol.innerHTML = '';
+    cells = [];
 }
 
 //will draw the a number x number board
@@ -59,22 +66,81 @@ function drawScreen(){
     //create row column of divs
     for(let i = 0; i < number; i++)
     {
-        let row = document.createElement("div");
-        row.className = "column-style";
+        let col = document.createElement("div");
+        col.className = "column-style";
         for(let j = 0; j < number; j++)
         {
-            let items = document.createElement("div");
-            items.className = "cellStyle";
-            items.addEventListener("mouseover", () => {
-                let currTile = window.getComputedStyle(items);
-                let currStyle = currTile.getPropertyValue("color");
-                getSelectedOption(currStyle);
-                items.style.backgroundColor = rgbColor;
-            });
-            row.appendChild(items);
+            //creates a new cell object
+            new Cell(col);
         }
-        parent.appendChild(row);
+        parent.appendChild(col);
     }
+}
+
+//each cell will contain the current color and will update if needed
+//it will also determine the grey scale number of passes. Will also update
+//collor depending on the pass number
+function Cell(obj){
+    this.pass = 0;
+
+    this.element = document.createElement("div");
+    this.element.className = "cellStyle";
+
+    //will be used to change the shades of black
+    cells.push(this);
+
+    this.element.addEventListener("mouseover", this);
+    obj.appendChild(this.element);
+}
+
+//will be used to call if mouse 'hovers' on cell
+Cell.prototype.handleEvent = function(obj){
+    switch(obj.type){
+        case "mouseover": this.mouseover(obj);
+    }
+}
+
+//change color of cell depending of what option has been 
+//selected from the dropdown.
+Cell.prototype.mouseover = function(obj){
+    //get what the current selected option is
+    let colorPixle = colorStyle.options[colorStyle.selectedIndex].value;
+    
+    //change color depending on what the selcted option is
+    if(colorPixle === "default")
+    {
+        this.element.style.backgroundColor = shadeBlack[9];
+        //if the greyscale has been changed, reset all passes
+        //from the pixel to 0
+        if(isGreyScale)
+            resetPasses();
+    }
+    else if(colorPixle === "greyscale")
+    {
+        //change the color to a diffrent shade of black
+        if(this.pass < 10)
+        {
+            this.element.style.backgroundColor = shadeBlack[this.pass];
+            this.pass++;
+        }
+        isGreyScale = true;
+    }
+    else if(colorPixle === "rainbow")
+    {
+        isGreyScale = false;//turns greyscale off just in case
+        this.element.style.backgroundColor = random_rgb();
+        //if the greyscale has been changed, reset all passes
+        //from the pixel to 0
+        if(isGreyScale)
+            resetPasses();
+    }
+}
+
+//will reset each cells 'pass value'
+function resetPasses(){
+    cells.forEach(e => e.pass = 0);
+
+    isGreyScale = false;
 }
 
 //will update the current resolution to a new one
@@ -83,39 +149,11 @@ function updateResolution(){
     resolution.insertAdjacentHTML('beforeend', str);
 }
 
-//will change pixel color on hover
-function updatePixelColor(){
-    pixelColor.style.backgroundColor = rgbColor;
+//returns random rgb color
+function random_rgb() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s)+')';
 }
-
-//will change the pixel color based on user selection
-function getSelectedOption(currStyle){
-    colorPixle = colorStyle.options[colorStyle.selectedIndex].value;
-    if(colorPixle === "default")
-    {
-        rgbColor = "rgb(0, 0, 0)";
-        passCount = 0;
-    }
-    else if(colorPixle === "greyscale")
-    {
-        rgbArr = getRGBArray(currStyle);
-        if(rgbArr[0] != 255 || rgbArr[1] != 255 || rgbArr[2] != 255 )
-        {
-            
-        }
-    }
-}
-
-//get rgb in index form
-function getRGBArray(rbgStr){
-    return rgb.replace(/[^\d,]/g, '').split(',');
-}
-
-function setGreyScale(currStyle)
-{
-    
-}
-
 
 //draw the screen when page is reloaded with default size
 //as well as display resolution and pixel color used when hover
